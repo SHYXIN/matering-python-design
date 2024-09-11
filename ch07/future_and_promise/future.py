a9a9c0001ae4ed28d8f8cd4b1dad2e551c4ee3c0
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2024-09-11 16:25:15 +0800

    第7章 Concurrency and Asynchronous Patterns

---
+++ ch07/future_and_promise/future.py
@@ -0,0 +1,15 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def square(x):
+    return x * x
+
+
+with ThreadPoolExecutor() as executor:
+    future1 = executor.submit(square, 2)
+    future2 = executor.submit(square, 3)
+    future3 = executor.submit(square, 4)
+
+    futures = [future1, future2, future3]
+
+    for future in as_completed(futures):
+        print(f"Result: {future.result()}")
